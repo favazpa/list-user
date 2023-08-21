fc5c91ccdc41618b5b02206725720cd16a817468
Favaz <favazindianarmy007@gmail.com>
2023-08-21 11:29:28 +0530

    Added loading indicator in Flatlist

--- src/screens/list-screen/index.js
+++ src/screens/list-screen/index.js
@@ -1,36 +1,58 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
 import {getUsers} from '../../utils/UserManager';
 import ListItem from './components/ListItem';
 import CustomNavigationHeader from './components/CustomNavigationHeader';
+import Colors from '../../themes/Colors';
 
 const ListHeaderComponent = () => {
   return <Text style={styles.header}>Select Profile</Text>;
 };
 
+const ListFooterComponent = loading =>
+  loading ? <ActivityIndicator size="large" color={Colors.primary} /> : null;
+
 const UserListScreen = ({navigation}) => {
   const [users, setUsers] = useState([]);
   const [currentBatchIndex, setCurrentBatchIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const displayedUsers = users.slice(0, (currentBatchIndex + 1) * 12);
+  const batchSize = 12;
 
   useEffect(() => {
     const fetchUsers = async () => {
+      setIsLoading(true);
       const usersData = await getUsers();
       setUsers(usersData);
+      setIsLoading(false);
     };
     fetchUsers();
   }, []);
 
   const showNextBatch = () => {
-    setCurrentBatchIndex(currentBatchIndex + 1);
+    if (!isLoading) {
+      const startIndex = (currentBatchIndex + 1) * batchSize;
+      if (startIndex < users.length) {
+        setIsLoading(true);
+        setTimeout(() => {
+          setCurrentBatchIndex(currentBatchIndex + 1);
+          setIsLoading(false);
+        }, 500);
+      }
+    }
   };
 
   return (
     <View style={styles.container}>
       <CustomNavigationHeader navigation={navigation} />
       <FlatList
-        data={displayedUsers}
+        data={users.slice(0, (currentBatchIndex + 1) * batchSize)}
         ListHeaderComponent={ListHeaderComponent}
         onEndReachedThreshold={0.01}
         onEndReached={showNextBatch}
@@ -38,6 +60,7 @@ const UserListScreen = ({navigation}) => {
         style={{flex: 1}}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({item}) => <ListItem details={item} />}
+        ListFooterComponent={ListFooterComponent(isLoading)}
       />
     </View>
   );
@@ -48,9 +71,8 @@ export default UserListScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EEEFF4',
+    backgroundColor: Colors.backgroundColor2,
   },
-
   header: {
     fontSize: 17,
     fontWeight: 'bold',
